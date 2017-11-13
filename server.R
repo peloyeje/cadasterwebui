8e3419ebe95f5f31449d7a5fa76f3ddc4a3e9108
Jean-Eudes Peloye <hello@peloye.je>
2017-11-13 23:08:01 +0100

    Move cadastertools dependency to github

--- server.R
+++ server.R
@@ -1,30 +1,32 @@
 library(shiny)
-library(cadastertools)
 library(assertthat)
 library(dplyr)
 library(leaflet)
 library(sf)
 
+library(devtools)
+install_github("peloyeje/cadastertools")
+
 shinyServer(function(input, output, session) {
   # Base variables
   layers <- c("batiments", "parcelles", "feuilles", "sections")
-  
+
   # Reactive values
   # ---
-  
+
   # Container from the draw mode
   draw <- reactiveValues(
     coords = list(),
     area_toggled = FALSE
   )
-  
+
   # When the search address form is submitted, search the input against the BANO database
   address_choices <- eventReactive(input$search, {
     # Check if the address is long enough
     validate(
       need(nchar(input$search_address) > 10, "L'adresse est trop courte")
     )
-    
+
     # Search on BANO
     tryCatch(
       as.data.frame(cadastertools::get_bano_matches(input$search_address)),
@@ -35,7 +37,7 @@ shinyServer(function(input, output, session) {
       }
     )
   })
-  
+
   # Get selected address from the dropdown list
   selected_address <- eventReactive(input$select, {
     validate(
@@ -44,7 +46,7 @@ shinyServer(function(input, output, session) {
 
     address_choices() %>% filter(id == input$selected_address)
   })
-  
+
   # Downloads all required files for the selected address and returns local file paths
   layer_paths <- reactive({
     sapply(layers, USE.NAMES = TRUE, simplify = FALSE, FUN = function(layer) {
@@ -63,7 +65,7 @@ shinyServer(function(input, output, session) {
       )
     }
   })
-  
+
   # Additionnal polygons
   complementary_data <- reactive({
     sapply(layer_paths()[-1], USE.NAMES = TRUE, simplify = FALSE, FUN = function(layer) {
@@ -73,7 +75,7 @@ shinyServer(function(input, output, session) {
       )
     })
   })
-  
+
   # Popin content with the additionnal information
   formatted_complementary_data <- reactive({
     glue::glue("<strong>parcelle number </strong>: {parcelle_number} with area {parcelle_area} </br>
@@ -85,10 +87,10 @@ shinyServer(function(input, output, session) {
                section_id = complementary_data()$sections$id
     )
   })
-  
+
   # Logic
   # ---
-  
+
   # Displays the addresses found on BANO in the select input
   output$choices <- renderUI({
     if (!is.null(address_choices())) {
@@ -121,20 +123,20 @@ shinyServer(function(input, output, session) {
 
   # Create the initial map
   output$map <- renderLeaflet({
-    leaflet() %>% 
-      addTiles() %>% 
+    leaflet() %>%
+      addTiles() %>%
       setView(2.3037, 46.4317, zoom = 6)
   })
 
   # Update the map each time nearest geo entry is updated
   observe({
     leafletProxy("map", data = nearest_building()$geometry) %>%
-      clearShapes() %>% 
-      clearPopups() %>% 
+      clearShapes() %>%
+      clearPopups() %>%
       addPolygons(color = "#444444", popup = formatted_complementary_data()) %>%
       setView(selected_address()$longitude, selected_address()$latitude, zoom = 18)
   })
-  
+
   # When there is a click on the map
   observeEvent(input$map_click, {
     # What follows only takes place in draw mode
@@ -142,18 +144,18 @@ shinyServer(function(input, output, session) {
       # Add new point to list
       point <- c(input$map_click$lng, input$map_click$lat)
       draw$coords[[length(draw$coords) + 1]] <- point
-      
+
       # Create polygon and render it on the map
       draw$polygon <- cadastertools::polygon_from_list(draw$coords)
       draw$area <- cadastertools::get_area(draw$polygon) %>% format(ndigits = 2, nsmall = 1)
-      
-      leafletProxy("map", data = draw$polygon) %>% 
-        clearShapes() %>% 
-        clearPopups() %>% 
+
+      leafletProxy("map", data = draw$polygon) %>%
+        clearShapes() %>%
+        clearPopups() %>%
         addPolygons(color = "#ff9999")
     }
   })
-  
+
   # When there is a click on a polygon
   observeEvent(input$map_shape_click, {
     # If we're in draw mode
@@ -163,14 +165,14 @@ shinyServer(function(input, output, session) {
         # If the tooltip isn't already showed
         if (!draw$area_toggled) {
           content <- glue::glue("<strong>Area</strong> : {area}", area = draw$area)
-          
-          leafletProxy("map") %>% 
+
+          leafletProxy("map") %>%
             addPopups(
-              input$map_shape_click$lng, 
-              input$map_shape_click$lat, 
-              content  
+              input$map_shape_click$lng,
+              input$map_shape_click$lat,
+              content
             )
-          
+
           draw$area_toggled = TRUE
         } else {
           leafletProxy("map") %>% clearPopups()
@@ -179,16 +181,16 @@ shinyServer(function(input, output, session) {
       }
     }
   })
-  
+
   # On toggle ...
   observeEvent(input$draw, {
-    # Reset values 
+    # Reset values
     draw$coords = list()
     draw$polygon = NULL
     draw$area_toggled = FALSE
     # Remove any polygon from view
-    leafletProxy("map") %>% 
-      clearShapes() %>% 
+    leafletProxy("map") %>%
+      clearShapes() %>%
       clearPopups()
   })
 })
